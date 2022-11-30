d36886285c7c8155005b1bc52d3c20175dd887c3
AnimBadger <Anim.ansah.stephen@gmail.com>
2022-11-30 01:53:46 +0000

    combo

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-for i in range(10):
-    for j in range(9):
-        print('{:d}{:d}'.format(i, j), end=', ')
-print(f'{i}{j + 1}')
+for i in range(0, 99):
+    print("{:02d}".format(i), end=", ")
+print(i + 1)
