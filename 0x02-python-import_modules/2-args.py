a2740ddeeb9e449d18991e658da91b613db39f92
AnimBadger <Anim.ansah.stephen@gmail.com>
2022-12-01 20:03:12 +0000

    argv fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 import sys
 if __name__ == '__main__':
-    n = len(sys.argv)
-    if n <= 1:
-        print('{:d} arguments.'.format(0))
-    elif (n == 2):
-        print('{:d} arguement:'.format(n - 1))
-        for i in range(1, n):
+    n = len(sys.argv) - 1
+    if n < 1:
+        print('{:d} arguments.'.format(n))
+    elif (n == 1):
+        print('{:d} arguement:'.format(n))
+        for i in range(1, n + 1):
             print('{:d}: {:s}'.format(i, sys.argv[i]))
     else:
-        print('{:d} arguments:'.format(n - 1))
-        for i in range(1, n):
+        print('{:d} arguments:'.format(n))
+        for i in range(1, n + 1):
             print('{:d}: {}'.format(i, sys.argv[i]))
