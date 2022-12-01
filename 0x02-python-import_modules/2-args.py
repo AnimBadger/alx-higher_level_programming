d199d9317c5fb7f6835c46fa32a393e1bc92745b
AnimBadger <Anim.ansah.stephen@gmail.com>
2022-12-01 19:52:12 +0000

    args fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -11,4 +11,4 @@ if __name__ == '__main__':
     else:
         print('{:d} arguments:'.format(n - 1))
         for i in range(1, n):
-            print('{:d}: {:s}'.format(i, sys.argv[i]))
+            print('{:d}: {}'.format(i, sys.argv[i]))
