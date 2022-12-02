8a804111e68a0a071ea1f05312353e24cb3309cf
AnimBadger <Anim.ansah.stephen@gmail.com>
2022-12-02 18:18:20 +0000

    requirement fix

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,6 +2,7 @@
 def print_reversed_list_integer(my_list=[]):
     if (not my_list):
         pass
-    my_list.reverse()
-    for i in range(len(my_list)):
-        print('{:d}'.format(my_list[i]))
+    else:
+        my_list.reverse()
+        for i in range(len(my_list)):
+            print('{:d}'.format(my_list[i]))
