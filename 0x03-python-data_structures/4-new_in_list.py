d033772b0d2180edbd1160cdf88ba76cddcac2bf
AnimBadger <Anim.ansah.stephen@gmail.com>
2022-12-02 18:23:02 +0000

    requirement fix

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,10 +2,9 @@
 def new_in_list(my_list, idx, element):
     if (idx < 0):
         return (my_list)
+    leng = len(my_list)
+    if (idx > leng - 1):
+        return (my_list)
     new_list = my_list.copy()
-    for i in range(len(new_list)):
-        if (i == idx):
-            new_list[i] = element
-            return (new_list)
-        elif (i > len(new_list)):
-            return (new_list)
+    new_list[idx] = element
+    return (new_list)
