e43e7479cf44f8b2891555c83184b9c2e703d9a2
AnimBadger <Anim.ansah.stephen@gmail.com>
2022-12-02 18:08:24 +0000

    requirement fix

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,8 +2,7 @@
 def element_at(my_list, idx):
     if idx < 0:
         return (None)
-    for i in range(len(my_list)):
-        if (i == idx):
-            return (my_list[i])
-        elif (i > len(my_list)):
-            return (None)
+    leng = len(my_list)
+    if (idx > leng - 1):
+        return (None)
+    return my_list[idx]
