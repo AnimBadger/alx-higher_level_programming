9871ab37e56fb4aefed5c70acf4bef9f6835d3b7
AnimBadger <Anim.ansah.stephen@gmail.com>
2022-12-07 20:57:39 +0000

    test

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    if key in a_dictionary.keys():
-        a_dictionary.get[key] = value
-    else:
-        a_dictionary.update({key: value})
+    a_dictionary.get[key] = value
+    return a_dictionary
