e6c7e570f849eeb15f8a878b11c9469482658c35
AnimBadger <Anim.ansah.stephen@gmail.com>
2022-12-07 22:02:30 +0000

    fix?

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
     new_dic = a_dictionary.copy()
-    listed = list(a_dictionary.key())
+    listed = list(a_dictionary.keys())
     for i in listed:
         new_dic[i] *= 2
     return new_dic
