3c4c4bb4ab30fbdac7a28737eff22e6772bb15bd
AnimBadger <Anim.ansah.stephen@gmail.com>
2022-12-07 22:09:19 +0000

    fix?

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if len(a_dictionary) <= 0:
+    if not a_dictionary:
         return None
-    else:
-        return max(a_dictionary)
+    return max(a_dictionary)
