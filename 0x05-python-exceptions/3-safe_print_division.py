1b65167714d0446eef9a5fedb49ac9474fd8e8f7
AnimBadger <Anim.ansah.stephen@gmail.com>
2022-12-19 16:47:27 +0000

    requirement fix

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -5,4 +5,4 @@ def safe_print_division(a, b):
     except ZeroDivisionError:
         result = None
     finally:
-        print('Inside result: {:d}'.format(result))
+        print('Inside result: {}'.format(result))
