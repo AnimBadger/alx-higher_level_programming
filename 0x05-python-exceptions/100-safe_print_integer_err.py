fce054d5bdcb3e0b5957ed67c02d6ad64f74811c
AnimBadger <Anim.ansah.stephen@gmail.com>
2022-12-19 18:42:22 +0000

    safe integer

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -7,3 +7,6 @@ def safe_print_integer_err(value):
         print('{:d}'.format(value))
     except (ValueError, TypeError) as err:
         print('Exception: {}'.format(err), file=sys.stderr)
+        return False
+    else:
+        return True
