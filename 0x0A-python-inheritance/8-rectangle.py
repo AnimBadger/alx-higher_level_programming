f46dbb864c4e06643eab3523384eb48047f87225
AnimBadger <Anim.ansah.stephen@gmail.com>
2023-01-09 18:04:42 +0000

    shallow fix

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -7,6 +7,7 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
+    '''Rectangle that inherits from BaseGeometry'''
     def __init__(self, width, height):
         BaseGeometry.integer_validator(self, 'height', height)
         self.__height = height
