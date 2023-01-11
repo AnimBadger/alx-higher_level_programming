7c3a403e9338b751f857662148bef0d1ef43e845
AnimBadger <Anim.ansah.stephen@gmail.com>
2023-01-11 15:15:17 +0000

    test cases fix

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -6,10 +6,9 @@ def matrix_divided(matrix, div):
     '''function to divide matrix'''
 
     err_type = 'matrix must be a matrix (list of lists) of integers/floats'
-    error_msg_size = 'Each row of the matrix must have \
-        the same size'
+    err_size = 'Each row of the matrix must have the same size'
 
-    if not isinstance(div, int) or not isinstance(div, float):
+    if type(div) not in (float, int):
         raise TypeError('div must be a number')
 
     if div == 0:
@@ -25,7 +24,7 @@ def matrix_divided(matrix, div):
             raise TypeError(err_type)
 
         if len_e != 0 and len(items) != len_e:
-            raise TypeError(error_msg_size)
+            raise TypeError(err_size)
 
         for in_item in items:
             if not type(in_item) in (int, float):
