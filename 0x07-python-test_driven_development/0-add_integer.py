86939d6e8af6b894cacb7174837c8f61324151ec
AnimBadger <Anim.ansah.stephen@gmail.com>
2023-01-11 14:12:39 +0000

    test cases fix

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 '''module to add two integers'''
+
+
 def add_integer(a, b=98):
     '''function that adds two integers'''
     if not isinstance(a, int) and not isinstance(a, float):
