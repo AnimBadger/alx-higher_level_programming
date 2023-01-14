ac7e2e6ef6fc3b08418b828e6da16dee972142ed
AnimBadger <Anim.ansah.stephen@gmail.com>
2023-01-14 20:19:02 +0000

    added csv classmethods

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -63,3 +63,41 @@ class Base:
                 return [cls.create(**d) for d in list_dict]
         except IOError:
             return []
+    
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        '''function that saves to CSV file'''
+        filename = cls.__name__ + '.csv'
+        with open(filename, 'w', newline="") as csv_file:
+            if list_objs is None or list_objs == []:
+                return csv_file.write('[]')
+            else:
+                if cls.__name__ == 'Rectangle':
+                    fieldnames = [
+                        'id','width','height','x','y'
+                    ]
+                else:
+                    fieldnames = ['id','size','x','y']
+                writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+                for lines in list_objs:
+                    writer.writerow(lines.to_dictionary())
+    
+    @classmethod
+    def load_from_file_csv(cls):
+        """Return a list of classes instantiated from a CSV file.
+        """
+        filename = cls.__name__ + ".csv"
+        try:
+            with open(filename, "r", newline="") as csv_file:
+                if cls.__name__ == "Rectangle":
+                    fieldnames = ["id", "width", "height", "x", "y"]
+                else:
+                    fieldnames = ["id", "size", "x", "y"]
+                list_dicts = csv.DictReader(csv_file, fieldnames=fieldnames)
+                list_dicts = [dict([k, int(v)] for k, v in d.items())
+                              for d in list_dicts]
+                return [cls.create(**d) for d in list_dicts]
+        except IOError:
+            return []
+
+            
