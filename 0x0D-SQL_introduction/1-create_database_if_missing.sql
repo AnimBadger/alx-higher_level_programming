21e0fc0cab3220ceb2ead281c812363f0495ba2f
AnimBadger <anim.ansah.stephen@gmail.com>
2023-02-14 22:57:48 +0000

    add backticks

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,3 +1,3 @@
 -- Create database if does not exist
 
-CREATE DATABASE IF NOT EXISTS 'hbtn_0c_0';
+CREATE DATABASE IF NOT EXISTS `hbtn_0c_0`;
--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,3 +1,3 @@
 -- remove database from server
 
-DROP DATABASE IF EXISTS 'hbtn_0c_0';
+DROP DATABASE IF EXISTS `hbtn_0c_0`;
