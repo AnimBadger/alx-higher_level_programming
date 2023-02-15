6fef6f1c8cd9e3bb1d42f367eb2cf21707336696
AnimBadger <anim.ansah.stephen@gmail.com>
2023-02-15 08:10:18 +0000

    more advanced

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,8 @@
+-- display top cities from table
+
+SELECT `city`, AVG(`value`) as `avg_temp`
+FROM `temperatures`
+WHERE `month` = 7 or `month` = 8
+GROUP BY `city`
+ORDER BY `avg_temp` DESC
+LIMIT 3;
---
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -0,0 +1,6 @@
+-- maximum state temprature grouped by state
+
+SELECT `state`, MAX(`value`) AS `max_temp`
+FROM `temperatures`
+GROUP BY `state`
+ORDER BY `state`;
