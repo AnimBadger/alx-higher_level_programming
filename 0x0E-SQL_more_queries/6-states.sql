bf7fded1eb37f8163fa982e70128fe80684bccce
AnimBadger <anim.ansah.stephen@gmail.com>
2023-02-16 14:21:12 +0000

    modify use

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,5 +1,6 @@
 -- create database with state table
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states(
     id int AUTO_INCREMENT NOT NULL,
     name VARCHAR(256) NOT NULL,
