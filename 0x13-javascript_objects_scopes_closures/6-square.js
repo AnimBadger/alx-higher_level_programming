9becf1abab8cf76563c8341e7f10d9950dfe3533
AnimBadger <anim.ansah.stephen@gmail.com>
2023-03-14 20:21:46 +0000

    squares

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const Rectangle = require("./5-square.js");
+
+class Square extends Rectangle {
+  charPrint(c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        console.log("C".repeat(this.width));
+      }
+    }
+  }
+}
+
+module.exports = Square;
