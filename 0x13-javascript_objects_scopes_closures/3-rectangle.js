01411a66fa43f7322d24e20b55acbf1fd47b4b7a
AnimBadger <anim.ansah.stephen@gmail.com>
2023-03-14 17:43:28 +0000

    semistandard fix

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,20 +1,21 @@
 #!/usr/bin/node
 
-class Rectangle{
-    constructor(w, h){
-        if (w > 0 && h > 0){
-            this.width = w
-            this.height = h
-        }
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
     }
-    print(){
-        for (let i = 0; i < this.height; i++){
-            for (let j = 0; j < this.width; j++){
-                process.stdout.write('X' + '');
-            }
-            process.stdout.write('\n');
-        }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        process.stdout.write('X' + '');
+      }
+      process.stdout.write('\n');
     }
+  }
 }
 
 module.exports = Rectangle;
