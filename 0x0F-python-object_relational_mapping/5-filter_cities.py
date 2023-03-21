5ece7becc4fba997cd14755d509f7b1877de1a32
AnimBadger <anim.ansah.stephen@gmail.com>
2023-03-21 12:50:50 +0000

    cities change

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,18 +1,29 @@
 #!/usr/bin/python3
-"""  lists all states from the database hbtn_0e_0_usa """
+""" Select states with names matching arguments """
+
+
+from sys import argv
 import MySQLdb
-import sys
 
+if __name__ == '__main__':
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
-    cur = db.cursor()
-    cur.execute("""SELECT cities.name FROM
-                cities INNER JOIN states ON states.id=cities.state_id
-                WHERE states.name=%s""", (sys.argv[4],))
-    rows = cur.fetchall()
-    tmp = list(row[0] for row in rows)
-    print(*tmp, sep=", ")
-    cur.close()
-    db.close()
+    db_user = argv[1]
+    db_passwd = argv[2]
+    db_name = argv[3]
+    user_state = argv[4]
+
+    database = MySQLdb.connect(host='localhost',
+                               port=3306,
+                               user=db_user,
+                               passwd=db_passwd,
+                               db=db_name)
+
+    cursor = database.cursor()
+
+    cursor.execute('SELECT cities.name FROM cities\
+                   JOIN states\
+                   ON cities.state_id = states.id\
+                   WHERE states.name = %s\
+                   ORDER BY states.id ASC', (user_state,))
+    result = []
+    print(', '.join([value[0] for value in cursor.fetchall()]))
