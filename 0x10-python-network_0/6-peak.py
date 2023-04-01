748e072d41f8d45ae6972e2b49dd304a7ffdc7b7
AnimBadger <anim.ansah.stephen@gmail.com>
2023-04-01 14:02:22 +0000

    efficiency test

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
-'''
-    function that finds the peak of an unsorted list
-'''
+"""Finds a peak in a list of unsorted integers"""
 
 
 def find_peak(list_of_integers):
-    '''find peak of list of integers'''
+    """Finds a peak in list_of_integers"""
+
     if list_of_integers is None or list_of_integers == []:
         return None
-    high = len(list_of_integers)
-    mid = int(len(list_of_integers) // 2)
-
-    if high == 1:
+    lo = 0
+    hi = len(list_of_integers)
+    mid = ((hi - lo) // 2) + lo
+    mid = int(mid)
+    if hi == 1:
         return list_of_integers[0]
-    if high == 2:
+    if hi == 2:
         return max(list_of_integers)
     if list_of_integers[mid] >= list_of_integers[mid - 1] and\
             list_of_integers[mid] >= list_of_integers[mid + 1]:
