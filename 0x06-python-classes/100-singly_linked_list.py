348e14509d0e0bcd6e0c088fcd8a8afb6cee3fa8
AnimBadger <anim.ansah.stephen@gmail.com>
2023-06-14 08:28:26 +0000

    updated classes

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,76 +1,76 @@
 #!/usr/bin/python3
-'''define classes for a singly-linked list'''
+"""Define classes for a singly-linked list."""
 
 
 class Node:
-    '''Represent a node in a singly-linked list'''
+    """Represent a node in a singly-linked list."""
 
-    def __init__(self, data, next_data=None):
-        '''Initialize a new Node
+    def __init__(self, data, next_node=None):
+        """Initialize a new Node.
 
         Args:
-                        data (int): The data of the new Node
-                        next_node (Node): The next node of the new Node
-        '''
-
+            data (int): The data of the new Node.
+            next_node (Node): The next node of the new Node.
+        """
         self.data = data
-        self.next_node = next_data
+        self.next_node = next_node
 
     @property
     def data(self):
-        '''Set the data of the Node'''
-        return self.__data
+        """Get/set the data of the Node."""
+        return (self.__data)
 
     @data.setter
     def data(self, value):
         if not isinstance(value, int):
-            raise TypeError('data must be an integer')
+            raise TypeError("data must be an integer")
         self.__data = value
 
     @property
     def next_node(self):
-        '''Set next_node of Node'''
-        return self.__next_node
+        """Get/set the next_node of the Node."""
+        return (self.__next_node)
 
     @next_node.setter
     def next_node(self, value):
         if not isinstance(value, Node) and value is not None:
-            raise TypeError('next_node must be a Node object')
+            raise TypeError("next_node must be a Node object")
         self.__next_node = value
 
 
 class SinglyLinkedList:
-    '''Represent a singly-linked list'''
+    """Represent a singly-linked list."""
 
     def __init__(self):
-        '''Initialize a new singly linked list'''
+        """Initalize a new SinglyLinkedList."""
         self.__head = None
 
     def sorted_insert(self, value):
-        '''Insert a new Node to the SinglyLinkedList
+        """Insert a new Node to the SinglyLinkedList.
 
         The node is inserted into the list at the correct
-        ordered numerical position
+        ordered numerical position.
 
         Args:
-                value (Node): The new Node to insert
-        '''
+            value (Node): The new Node to insert.
+        """
         new = Node(value)
         if self.__head is None:
-            new.__next_node = None
+            new.next_node = None
             self.__head = new
         elif self.__head.data > value:
             new.next_node = self.__head
             self.__head = new
         else:
             tmp = self.__head
-            while (tmp.next_node is not None and tmp.next_node.data < value):
+            while (tmp.next_node is not None and
+                    tmp.next_node.data < value):
                 tmp = tmp.next_node
             new.next_node = tmp.next_node
             tmp.next_node = new
 
     def __str__(self):
-        '''define the print() representation of SinglyLinkedList'''
+        """Define the print() representation of a SinglyLinkedList."""
         values = []
         tmp = self.__head
         while tmp is not None:
--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -28,8 +28,8 @@ class Square:
         Args:
             value (int): size of the square.
         """
-        if type(value) is not int:
-            raise TypeError('size must be an integer')
+        if type(value) is not int and type(value) is not float:
+            raise TypeError('size must be a number')
         elif value < 0:
             raise ValueError('size must be >= 0')
         else:
@@ -43,13 +43,17 @@ class Square:
         """
         return self.__size**2
 
-    def my_print(self):
-        """prints in stdout the square with the character #"""
+    def __lt__(self, other):
+        return self.size < other.size
 
-        if self.__size != 0:
-            for i in range(self.__size):
-                for j in range(self.__size):
-                    print('#', end='')
-                print()
-        else:
-            print()
+    def __le__(self, other):
+        return self.size <= other.size
+
+    def __eq__(self, other):
+        return self.size == other.size
+
+    def __ne__(self, other):
+        return self.size != other.size
+
+    def __ge__(self, other):
+        return self.size >= other.size
