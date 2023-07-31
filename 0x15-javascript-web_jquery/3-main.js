06f1e3819242f4f8941c204cb47472cf54ffd161
Stephen Ansah Anim <anim.ansah.stephen@gmail.com>
2023-07-31 20:01:05 +0000

    Delete 0x15-javascript-web_jquery directory

--- 0x15-javascript-web_jquery/3-main.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable */
-$(document).ready(function ($) {
-  $('#red_header').click(function () {
-    $('header').addClass('red');
-  });
-});
--- 0x15-javascript-web_jquery/README.md
+++
@@ -1,1 +0,0 @@
-Learning about JQuery and Ajax
